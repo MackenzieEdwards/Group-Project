e1d07660053d3ef1ced7791b85cae90d65e1d6c1
mackenzie.edwards <mackenzie.edwards@MU-C3-14.mullen.pvt.k12.co.us>
2014-10-14 14:02:42 -0600

    school

--- Group Project/src/VendingMachine.java
+++ Group Project/src/VendingMachine.java
@@ -1,12 +1,19 @@
-import java.lang.Object;
 import java.util.Scanner;
+
 
 public class VendingMachine
 	{
 	// A Cashier object for the Vending Machine.
-		private Cashier cashier;
-		// An InventoryManager object for the Vending Machine.
-		private InventoryManager im;
-		// A Scanner object to get keyboard input.
-		private Scanner keypad;
+	private Cashier cashier;
+	// An InventoryManager object for the Vending Machine.
+	private InventoryManager im;
+	// A Scanner object to get keyboard input.
+	private Scanner keypad;
+	
+	public static void main(String[] args)
+		{
+
+
+		}
+
 	}
